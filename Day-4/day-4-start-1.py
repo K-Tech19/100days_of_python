44e674e2f9e63a94fb51a120d7bbd9c811d4fcfb
K-tech19 <kennyjean561@gmail.com>
2021-09-16 11:19:45 -0400

    day-4-start

---
+++ Day-4/day-4-start-1.py
@@ -0,0 +1,20 @@
+import random # python module
+# import my_module #import code from created my_module.py file
+# print(my_module.pi)
+
+random_integer = random.randint(1,25)
+
+# print(random_integer)
+
+#0.0000000 - 0.9999999
+# random_float = random.random()
+# random_float = random.uniform(1,5)
+
+
+# print(random_float)
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")
+
+# random_float * 5
+# 0.00000 - 4.99999
